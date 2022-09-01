e07a88fff33e09d31e807d5271ed266857c45b3a
Ben Martin <1350682+bm9k@users.noreply.github.com>
2022-09-01 17:36:07 +1000

    Vectors

--- 01_banas_intro/src/main.rs
+++ 01_banas_intro/src/main.rs
@@ -237,6 +237,29 @@ fn _enums() {
     )
 }
 
+fn _vectors() {
+    // can grow if mutable & can only contain values of the same type
+    let vec1: Vec<i32> = Vec::new();
+    let mut vec2: Vec<i32> = vec![1, 2, 3, 4];
+    vec2.push(5);
+    println!("vec2's first elem: {}", vec2[0]);
+    let second: &i32 = &vec2[1];
+    match vec2.get(1) {
+        Some(second) => println!("2nd: {}", second),
+        None => println!("No 2nd value")
+    }
+
+    for i in &mut vec2 {
+        *i *= 2;
+    }
+
+    for i in &vec2 {
+        println!("{}", i);
+    }
+    println!("Vector length: {}", vec2.len());
+    println!("Pop: {:?}", vec2.pop());
+}
+
 fn main() {
     // _basic_ui();
     // _numerics();
@@ -247,5 +270,6 @@ fn main() {
     // _tuples();
     // _strings();
     // _casting();
-    _enums();
+    // _enums();
+    _vectors();
 }
