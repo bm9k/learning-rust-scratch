8568b3bfb8fb5c6fa3d980fa506aac8c4fa38152
Ben Martin <1350682+bm9k@users.noreply.github.com>
2022-09-01 11:58:27 +1000

    Add numeric types info

--- 01_banas_intro/src/main.rs
+++ 01_banas_intro/src/main.rs
@@ -13,4 +13,18 @@ fn main() {
     age += 1;
     // subtle much?
     println!("I'm {} and I want ${}", age, ONE_MIL);
+
+    println!("Unsigned integers: u8, u16, u32, u64, u128, usize");
+    println!("  - usize depends on computer");
+    println!("Signed integers: i8, i16, i32, i64, i128, isize");
+    println!("  - isize depends on computer");
+    println!("Floats: f8, f16, f32, f64, f128 (no fsize)");
+
+    println!();
+    println!("Max u32: {}", u32::MAX);
+    println!("Max u64: {}", u64::MAX);
+    println!("Max usize: {}", usize::MAX);
+    println!();
+    println!("Max f32: {}", f32::MAX);
+    println!("Max f64: {}", f64::MAX);
 }
