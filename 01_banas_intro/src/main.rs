9a5522255aa40bea432bfbac80723f18764a5e3a
Ben Martin <1350682+bm9k@users.noreply.github.com>
2022-09-01 12:12:16 +1000

    Move earlier code to separate functions & fp-precision & arithmetic operations

--- 01_banas_intro/src/main.rs
+++ 01_banas_intro/src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-fn main() {
+fn _basic_ui() {
     println!("What is your name?");
     let mut name = String::new();
     io::stdin().read_line(&mut name).expect("Did not receive input");
@@ -13,7 +13,9 @@ fn main() {
     age += 1;
     // subtle much?
     println!("I'm {} and I want ${}", age, ONE_MIL);
+}
 
+fn _numerics() {
     println!("Unsigned integers: u8, u16, u32, u64, u128, usize");
     println!("  - usize depends on computer");
     println!("Signed integers: i8, i16, i32, i64, i128, isize");
@@ -27,4 +29,33 @@ fn main() {
     println!();
     println!("Max f32: {}", f32::MAX);
     println!("Max f64: {}", f64::MAX);
+}
+
+fn _float_precision_and_arithmetic() {
+    // leading _ tells rust compiler to ignore unused variable errors (useful for placeholders)
+    let _flag = true;
+    let my_grade = 'c';
+
+    // floating point precision
+    let num_0 = "1.11111111111111111111";
+    println!("lit: {}", num_0);
+    let num_1: f32 = 1.11111111111111111111;
+    println!("f32: {}", num_1);
+    let num_2: f64 = 1.11111111111111111111;
+    println!("f64: {}", num_2);
+
+    // arithmetic
+    let a: u32 = 5;
+    let b: u32 = 4;
+    println!("5 + 4 = {}", a + b);
+    println!("5 - 4 = {}", a - b);
+    println!("5 * 4 = {}", a * b);
+    println!("5 / 4 = {}", a / b);
+    println!("5 % 4 = {}", a % b);
+}
+
+fn main() {
+    // _basic_ui();
+    // _numerics();
+    _float_precision_and_arithmetic();
 }
