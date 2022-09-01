3b0563ab6b267b32615549a19f433abfa2478318
Ben Martin <1350682+bm9k@users.noreply.github.com>
2022-09-01 11:45:29 +1000

    More ui

--- 01_banas_intro/src/main.rs
+++ 01_banas_intro/src/main.rs
@@ -5,4 +5,12 @@ fn main() {
     let mut name = String::new();
     io::stdin().read_line(&mut name).expect("Did not receive input");
     println!("Hello, {}, nice to meet you!", name.trim_end());
+
+    const ONE_MIL: u32 = 1_000_000;
+    // shadowing
+    let age = "47";
+    let mut age: u32 = age.trim().parse().expect("Age wasn't assigned a number");
+    age += 1;
+    // subtle much?
+    println!("I'm {} and I want ${}", age, ONE_MIL);
 }
