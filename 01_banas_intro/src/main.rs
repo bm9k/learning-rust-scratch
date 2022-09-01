f81da8310e4c77e6a4d797590cd07ee79c7b88a9
Ben Martin <1350682+bm9k@users.noreply.github.com>
2022-09-01 11:39:58 +1000

    User input

--- 01_banas_intro/src/main.rs
+++ 01_banas_intro/src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Did not receive input");
+    println!("Hello, {}, nice to meet you!", name.trim_end());
 }
