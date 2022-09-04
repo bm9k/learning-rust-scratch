f16260a4db56cf7c74b0a1c5add40ba96b64c1bf
Ben Martin <1350682+bm9k@users.noreply.github.com>
2022-09-05 07:59:01 +1000

    Add message struct

--- 04_realtime_chat_app/src/main.rs
+++ 04_realtime_chat_app/src/main.rs
@@ -1,13 +1,28 @@
+use rocket::tokio::sync::broadcast::channel;
+use rocket::serde::{Serialize, Deserialize};
+
 // imports rocket macros globally
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/ping")]
 fn ping() -> &'static str {
     "pong"
 }
 
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    #[field(validate = len(..30))]
+    pub room: String,
+    #[field(validate = len(..20))]
+    pub username: String,
+    pub message: String,
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .manage(channel::<Message>(1024).0)
         .mount("/", routes![ping])
 }
