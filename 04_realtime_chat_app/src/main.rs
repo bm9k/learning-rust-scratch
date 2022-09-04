14452654eb51346d8c3ad67a66b14f92459525fe
Ben Martin <1350682+bm9k@users.noreply.github.com>
2022-09-05 08:25:47 +1000

    Add get & post routes

--- 04_realtime_chat_app/src/main.rs
+++ 04_realtime_chat_app/src/main.rs
@@ -1,5 +1,11 @@
-use rocket::tokio::sync::broadcast::channel;
-use rocket::serde::{Serialize, Deserialize};
+use rocket::form::Form;
+use rocket::fs::{FileServer, relative};
+use rocket::response::stream::{Event, EventStream};
+use rocket::serde::{Deserialize, Serialize};
+use rocket::tokio::select;
+use rocket::tokio::sync::broadcast::{channel, error::RecvError, Sender};
+use rocket::Shutdown;
+use rocket::State;
 
 // imports rocket macros globally
 #[macro_use]
@@ -20,9 +26,35 @@ struct Message {
     pub message: String,
 }
 
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    // A send fails if there are no active subscribers, but that's ok
+    let _res = queue.send(form.into_inner());
+}
+
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&msg);
+        }
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .manage(channel::<Message>(1024).0)
-        .mount("/", routes![ping])
+        .mount("/", routes![ping, post, events])
 }
